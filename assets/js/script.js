73819d64a5835170fbed603c4d539dd01bff89ef
Neal Chris <tmbarnhart1@gmail.com>
2023-08-30 20:35:58 -0400

    Updates to nav, recipe cards

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function saveRecipe(recipeName) {
+    // Check if local storage is supported by the browser
+    if (typeof(Storage) !== "undefined") {
+        // Retrieve existing saved recipes or initialize an empty array
+        let savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
+        
+        // Check if the recipe is already saved
+        if (!savedRecipes.includes(recipeName)) {
+            savedRecipes.push(recipeName); // Add the recipe to the list
+            localStorage.setItem('savedRecipes', JSON.stringify(savedRecipes)); // Save the updated list
+            alert('Recipe saved successfully!');
+        } else {
+            alert('Recipe is already saved.');
+        }
+    } else {
+        alert('Local storage is not supported by your browser.');
+    }
+}
